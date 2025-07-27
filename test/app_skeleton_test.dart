95413c97b5cabbb33f6f4892ee374ff2e2e40d07
Adedeji Somoye <adedejisomoye1@gmail.com>
2025-07-27 22:33:04 +0100

    add widget testing

--- test/app_skeleton_test.dart
+++ test/app_skeleton_test.dart
@@ -2,21 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:product_viewer_app/app_skeleton.dart';
+import 'package:product_viewer_app/src/view/favorites/favorites.dart';
+import 'package:product_viewer_app/src/view/products/product_list_screen.dart';
 
 void main() {
-  testWidgets('BottomNavigationBar switches between Home and Favorites', (
+  testWidgets('BottomNavigationBar navigates between Home and Favorites', (
     WidgetTester tester,
   ) async {
     await tester.pumpWidget(
       const ProviderScope(child: MaterialApp(home: AppSkeleton())),
     );
 
-    expect(find.text('Favorites'), findsNothing);
-    expect(find.byType(BottomNavigationBar), findsOneWidget);
+    expect(find.byType(ProductListScreen), findsOneWidget);
+    expect(find.byType(FavoritesScreen), findsNothing);
 
     await tester.tap(find.byIcon(Icons.favorite));
     await tester.pumpAndSettle();
 
-    expect(find.text('Favorites'), findsOneWidget);
+    expect(find.byType(FavoritesScreen), findsOneWidget);
+    expect(find.byType(ProductListScreen), findsNothing);
+
+    await tester.tap(find.byIcon(Icons.home));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(ProductListScreen), findsOneWidget);
+    expect(find.byType(FavoritesScreen), findsNothing);
   });
 }
